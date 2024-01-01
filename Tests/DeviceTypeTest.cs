bc67b01e774fb5e27c0691148efc3d5315168ed9
Rodion.Skopych@e-consulting.com.ua <Rodion.Skopych@e-consulting.com.ua>
2024-01-01 18:50:30 +0200

    RemoveSomeTests

--- Tests/DeviceTypeTest.cs
+++ Tests/DeviceTypeTest.cs
@@ -16,27 +16,5 @@ namespace Tests
             //Assert
             Assert.True(result);
         }
-
-        [Fact]
-        public void TestIOSDevice()
-        {
-            //Arrange
-            Guid IOSId = new Guid("B5A648BA-262F-47BF-A0F5-4FDF68AC0C12");
-            //Actual
-            bool result = new Regex(ValidatePaterns.Guid).IsMatch(IOSId.ToString());
-            //Assert
-            Assert.True(result);
-        }
-
-        [Fact]
-        public void TestIOSDeviceWith1UpperCaseLetter()
-        {
-            //Arrange
-            Guid IOSId = new Guid("76Cb8c23-13a0-41e6-ad16-a2056ece43ba");
-            //Actual
-            bool result = new Regex(ValidatePaterns.Guid).IsMatch(IOSId.ToString());
-            //Assert
-            Assert.True(result);
-        }
     }
 }
